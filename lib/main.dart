f816dba3c69edcd4304865ae1d35436cb94c241c
yami-beta <yami-beta@users.noreply.github.com>
2021-08-04 19:51:59 +0900

    Step 4: Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
@@ -29,9 +22,51 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    /*
+       ListView.builder は無限リスト等を作る場合など、表示要素が不定の場合に利用する
+     */
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      /*
+         itemBuilder は行ごとに呼び出される
+       */
+      itemBuilder: (context, i) {
+        // リストアイテムの境界線は itemBuilder の奇数のときに配置している
+        if (i.isOdd) return const Divider();
+
+        /*
+           2で割った結果を int で返す
+           itemBuilder の i は Divider の表示でも使うため _suggestions の index は2で割った整数値になる
+         */
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
